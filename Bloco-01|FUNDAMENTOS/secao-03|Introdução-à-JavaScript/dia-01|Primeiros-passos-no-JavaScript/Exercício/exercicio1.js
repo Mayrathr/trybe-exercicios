75806dc8b53380fb3ac3f4683fc43d676a663798
Mayra Ramos <thaumaram@gmail.com>
2023-06-01 12:45:54 -0400

    exercicio1

---
+++ Bloco-01|FUNDAMENTOS/secao-03|Introdução-à-JavaScript/dia-01|Primeiros-passos-no-JavaScript/Exercício/exercicio1.js
@@ -0,0 +1,16 @@
+const num1 = 7;
+const num2 = 5;
+
+let adicao = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let modulo = num1 % num2;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
+
+
