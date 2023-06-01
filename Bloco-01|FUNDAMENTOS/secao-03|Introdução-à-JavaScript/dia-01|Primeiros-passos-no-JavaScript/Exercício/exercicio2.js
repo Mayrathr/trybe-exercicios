cad27fc9b50e18dc1c995880b3f5eef9ee6b267f
Mayra Ramos <thaumaram@gmail.com>
2023-06-01 13:16:13 -0400

    exercicio2

---
+++ Bloco-01|FUNDAMENTOS/secao-03|Introdução-à-JavaScript/dia-01|Primeiros-passos-no-JavaScript/Exercício/exercicio2.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 13;
+
+
+if (num1 > num2){
+  console.log(num1 + "é maior que" + num2);
+} else if (num1 === num2) {
+  console.log("Os dois números são iguais.");
+} else{
+  console.log(num2 + " é maior que " + num1);
+};
