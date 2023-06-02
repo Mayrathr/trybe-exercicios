2a89a321ed3b4a8af47277fd80e5872c2ea15e43
Mayra Ramos <thaumaram@gmail.com>
2023-06-01 21:40:13 -0400

    exercicio6

---
+++ Bloco-01|FUNDAMENTOS/secao-03|Introdução-à-JavaScript/dia-01|Primeiros-passos-no-JavaScript/exercicio6.js
@@ -0,0 +1,11 @@
+const num1 = 5;
+const num2 = 6;
+const num3 = 7;
+
+let numPar = false;
+
+if ((num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0 )){
+  numPar = true;
+};
+
+console.log(numPar);
