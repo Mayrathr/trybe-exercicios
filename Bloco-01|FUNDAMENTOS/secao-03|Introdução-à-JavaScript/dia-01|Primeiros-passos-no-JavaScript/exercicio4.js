8c1e72547cf19e1dc821d562d8e2e3cdadbf84a9
Mayra Ramos <thaumaram@gmail.com>
2023-06-01 21:26:22 -0400

    exercicio4

---
+++ Bloco-01|FUNDAMENTOS/secao-03|Introdução-à-JavaScript/dia-01|Primeiros-passos-no-JavaScript/exercicio4.js
@@ -0,0 +1,12 @@
+let angulo1 = 90;
+let angulo2 = 45;
+let angulo3 = 50;
+
+let somaAngulosTri = angulo1 + angulo2 + angulo3;
+let anguloTriangulo = 180;
+
+if (somaAngulosTri===180){
+  console.log(true);
+} else{
+  console.log(false);
+};
