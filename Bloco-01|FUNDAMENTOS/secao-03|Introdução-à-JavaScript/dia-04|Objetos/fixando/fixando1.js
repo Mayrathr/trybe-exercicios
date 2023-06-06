b7a0eca712c77ada03c7ab066ca723cb3dc837f3
Mayra Ramos <thaumaram@gmail.com>
2023-06-05 21:49:08 -0400

    ex.2-fixando1

--- Bloco-01|FUNDAMENTOS/secao-03|Introdução-à-JavaScript/dia-04|Objetos/fixando/fixando1.js
+++ Bloco-01|FUNDAMENTOS/secao-03|Introdução-à-JavaScript/dia-04|Objetos/fixando/fixando1.js
@@ -8,4 +8,7 @@ const player = {
   },
   bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
 };
-console.log(player.medals.silver)
+
+let message = `A jogadora ${player.name} tem ${player.age} anos de idade.`;
+
+console.log(message);
